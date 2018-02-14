668b911ab8ac5bc9da374ba01ed9154822b6c2e0
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2018-02-14 09:29:29 -0500

    nani

--- Lab3.3/src/ArrayMethods3.java
+++ Lab3.3/src/ArrayMethods3.java
@@ -28,11 +28,14 @@ If r > l
 		int left = 0; //the first position? which would just be zero i guess?
 		int right = list.length; //the last position or the right most number.
 		int middle = (left + right)/2; //find the middle to begin splitting in half
-	}
-	
-	public static int partition(int[] list1, int front, int back)
-	{
 		
+		//base case
+		if (list.length == 1)
+		{
+			return list;
+		}
+		//recursion
+		return (merge(mergeSort(left), mergeSort(right)));
 	}
 	
 	public static void quickSort(int[] list1, int front, int back)
@@ -40,7 +43,57 @@ If r > l
 		
 	}
 	
+		public static int partition(int[] list1, int front, int back)
+		{
+		
+		}
 	
+	
+	
+	
+	//merge from Lab3.2
+	public static String[] merge(String[] list1, String[] list2)
+	{
+		int x = list1.length;
+		int y = list2.length;
+		int z = x + y;
+		String sorted[] = new String [z];
+		int a= 0; //counting variable for list1
+		int b = 0; //counting variable for list2
+		int c = 0; //counting variable for the sorted array
+		
+		while(a < x && b < y) //makes sure it doesn't count out of bounds
+		{
+			if(list1[a].compareTo(list2[b]) <= 0 )
+			{
+				sorted[c] = list1[a];
+				c++;
+				a++;
+			} else if(list1[a].compareTo(list2[b]) > 0) {
+				sorted[c] = list2[b];
+				c++;
+				b++;
+			}
+		}
+		
+		//following two while loops is to fill in any leftover numbers
+		while (a < x)
+		{
+			sorted[c] = list1[a];
+			a++;
+			c++;
+		}
+		
+		while(b < y)
+		{
+			sorted[c] = list2[b];
+			b++;
+			c++;
+		}
+		
+		return sorted;
+		
+	}
 	//copying the array every time it splits 
 	public static int[] copyOfRange(int[] original_array, int from_index, int to_index)
 	{
